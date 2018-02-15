fdef5e1587d689d46e02294a03f4c7aaa8fb862f
Asus <Asus@DESKTOP-TTS3KL9>
2018-02-15 20:09:05 +0100

    add option list in form
    dodanie list rozwijalnych zaczytywanych z bazy, co ułatwia skladanie requestu.
    dodanie opisów w kontrolerze

--- src/main/java/com/cloud/telemetry/demo/RequestTypeController.java
+++ src/main/java/com/cloud/telemetry/demo/RequestTypeController.java
@@ -22,15 +22,20 @@ public class RequestTypeController {
     @Autowired
     UnitRequestRepo unitRequestRepo;
 
+    @Autowired
+    UnitRepo unitRepo;
+
     @RequestMapping( value="/request" , method = RequestMethod.GET)
     public ModelAndView req(Model model) {
 
         List<RequestType> requestype = requestTypeRepo.getAllTypeReq();
+        model.addAttribute("listaTypow", requestype);
 
         List<UnitRequest> requesty = unitRequestRepo.getNew10Req();
-
-        model.addAttribute("listaTypow", requestype);
         model.addAttribute("listaRequestow", requesty);
+
+        List<Units> unity = unitRepo.getUnits();
+        model.addAttribute("listaUnitow", unity);
 
         return new ModelAndView("request", "command", new NewRequest());
 }
@@ -41,19 +46,26 @@ public class RequestTypeController {
     public ModelAndView addRequest(Model model,NewRequest polecenie) {
 
 
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis()); //generowany czas wpisania requestu do bazy
 
-        UnitRequest r;
+        UnitRequest r; //wypełnianie nowej instancji encji UnitRequest
         r = new UnitRequest();
         r.setUnitNetIdent(polecenie.unit_net_ident);
         r.setRequestType(polecenie.request_type);
         r.setRegDate(timestamp);
         r.setUnit_input_id(polecenie.unit_input_id);
         r.setValue(polecenie.value);
-        unitRequestRepo.saveAndFlush(r);
+        unitRequestRepo.saveAndFlush(r); //zapis do bazy
 
+        //przekierowanie  na strone request i wypełnienie modelu danymi potrzebnymi na stronie
         List<UnitRequest> requesty = unitRequestRepo.getNew10Req();
         model.addAttribute("listaRequestow", requesty);
+
+        List<RequestType> requestype = requestTypeRepo.getAllTypeReq();
+        model.addAttribute("listaTypow", requestype);
+
+        List<Units> unity = unitRepo.getUnits();
+        model.addAttribute("listaUnitow", unity);
 
         return new ModelAndView("request", "command", new NewRequest());
     }
